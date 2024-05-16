2180bdb559fe20e1886fc65a2cf343046c5db4e0
melissaa7 <21457@students.mrgs.school.nz>
2024-05-16 20:51:19 +1200

    login page without logo

--- main.py
+++ main.py
@@ -1,10 +1,36 @@
 from tkinter import*
 
+#Function to print username and password
+def login():
+    username = username_entry.get()
+    password = password_entry.get()
+    print("Username: ", username)
+    print("Password: ", password)
+
+#Creating the main window
 window=Tk()
 window.title("Cash Control")
 
-window.grid()
-hello_label = Label(text="username")
-hello_label.grid(column=0, row=0)
+#size of window
+window.geometry("400x300")
+
+#Logo
+
+
+#Username
+username_label = Label(window, text="Username: ")
+username_label.pack()
+username_entry = Entry(window)
+username_entry.pack()
+
+#Password
+password_label = Label(window, text="Password: ")
+password_label.pack()
+password_entry = Entry(window)
+password_entry.pack()
+
+#Login Button
+login_button = Button(window,text="Login", command=login)
+login_button.pack()
 
 window.mainloop()
