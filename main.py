f86fcc832c081d38504b47babf57b24070cfebbb
melissaa7 <21457@students.mrgs.school.nz>
2024-05-23 13:46:34 +1200

    Adding background color to logo and added another file

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from tkinter import*
+from app_settings import*
 
 #Function to print username and password
 def login():
@@ -14,28 +15,25 @@ window.title("Cash Control")
 #size of window
 window.geometry("500x500")
 
-#set the background gradient
-
-
 #Logo
 logo_image = PhotoImage(file="Images/Logo.png")
-logo_label = Label(window, image=logo_image)
-logo_label.pack()
+logo_label = Label(window, image=logo_image, bg="white")
+logo_label.place(relx=0.5, rely=0.3, anchor=CENTER)
 
 #Username
 username_label = Label(window, text="Username: ")
-username_label.pack()
+username_label.place(relx=0.5, rely=0.5, anchor=CENTER)
 username_entry = Entry(window)
-username_entry.pack()
+username_entry.place(relx=0.5, rely=0.55, anchor=CENTER)
 
 #Password
 password_label = Label(window, text="Password: ")
-password_label.pack()
+password_label.place(relx=0.5, rely=0.6, anchor=CENTER)
 password_entry = Entry(window, show="*")
-password_entry.pack()
+password_entry.place(relx=0.5, rely=0.65, anchor=CENTER)
 
 #Login Button
-login_button = Button(window,text="ENTER", command=login)
-login_button.pack()
+login_button = Button(window,text="ENTER", command=login, bg="#CB997E")
+login_button.place(relx=0.5, rely=0.75, anchor=CENTER)
 
 window.mainloop()
